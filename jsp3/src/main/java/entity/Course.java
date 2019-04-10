d1b590491fabe5f30d63c5f0127d8efbdc821ed4
wangguoqiang <bsg_2008@outlook.com>
2019-04-10 17:41:16 +0800

    Eclipse发布项目到 github上

---
+++ jsp3/src/main/java/entity/Course.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Course implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	       private Integer couresId;
+	       private String name;
+	       private Integer days;
+	       
+		public Integer getId() {
+			return couresId;
+		}
+		public void setId(Integer couresId) {
+			this.couresId = couresId;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Integer getDays() {
+			return days;
+		}
+		public void setDays(Integer days) {
+			this.days = days;
+		}
+	       
+	       
+}
